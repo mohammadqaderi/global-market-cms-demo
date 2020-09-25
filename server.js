87372423501d040720056d40d226424792ff5515
mohammadqaderi <mqaderi44@gmail.com>
2020-09-25 15:26:01 +0300

    first scaffolding commit 25/5/2020

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/frenzy-dashboard-demo'));
+app.use(express.static('./dist/Global-Market-CMS'));
 
 app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: 'dist/Global-Market-CMS' }
